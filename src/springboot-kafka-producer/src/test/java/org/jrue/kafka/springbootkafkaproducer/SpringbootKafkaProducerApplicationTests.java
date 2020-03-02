7b93fe658016b8699a6fb0623443e5effa75fae4
root <ruelosjoel@gmail.com>
2020-03-02 09:30:54 -0600

    Initial commit for springboot-kafka-producer

---
+++ src/springboot-kafka-producer/src/test/java/org/jrue/kafka/springbootkafkaproducer/SpringbootKafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jrue.kafka.springbootkafkaproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
