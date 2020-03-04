15ca9309f2c86e8f35fd19b5f0df11a1baf5556b
root <ruelosjoel@gmail.com>
2020-03-04 09:02:30 -0600

    Added project springboot-kafka-consumer

--- src/springboot-kafka-producer/src/main/java/org/jrue/kafka/springbootkafkaproducer/BookResource.java
+++ src/springboot-kafka-producer/src/main/java/org/jrue/kafka/springbootkafkaproducer/BookResource.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("books")
 public class BookResource {
