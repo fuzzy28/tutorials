fc50941d9c128bc076a0e12d8683e4d6ac54da6a
root <ruelosjoel@gmail.com>
2020-04-10 02:52:33 -0500

    Initial commit for config-client

---
+++ src/springboot-config-client/src/test/java/org/jrue/springbootconfigclient/SpringbootConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jrue.springbootconfigclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootConfigClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
