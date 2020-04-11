bca8dc473db2a021028d15dc0d5ef646362b2a8a
root <ruelosjoel@gmail.com>
2020-04-11 11:09:19 -0500

    Initial commit for springcloud eureka server

---
+++ src/springboot-eureka-server/src/test/java/org/jrue/springbooteurekaserver/SpringbootEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jrue.springbooteurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
