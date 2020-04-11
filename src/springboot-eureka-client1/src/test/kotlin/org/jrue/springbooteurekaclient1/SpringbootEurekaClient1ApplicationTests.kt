3240c584483a759252f45cf1bc05f2cab43f11c0
root <ruelosjoel@gmail.com>
2020-04-11 11:21:09 -0500

    Initial commit for springcloud eureka client

---
+++ src/springboot-eureka-client1/src/test/kotlin/org/jrue/springbooteurekaclient1/SpringbootEurekaClient1ApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.jrue.springbooteurekaclient1
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootEurekaClient1ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
