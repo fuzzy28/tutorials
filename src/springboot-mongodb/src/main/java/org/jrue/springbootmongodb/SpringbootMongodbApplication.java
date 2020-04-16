385e07ca6e6cf4da9fcf661c051a673da181fd97
root <ruelosjoel@gmail.com>
2020-04-15 22:32:22 -0500

    Initial commit for spingboot-mongodb

---
+++ src/springboot-mongodb/src/main/java/org/jrue/springbootmongodb/SpringbootMongodbApplication.java
@@ -0,0 +1,13 @@
+package org.jrue.springbootmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMongodbApplication.class, args);
+	}
+
+}
---
+++ src/springboot-mongodb/src/test/java/org/jrue/springbootmongodb/SpringbootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jrue.springbootmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
